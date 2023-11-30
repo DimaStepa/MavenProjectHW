300e06e118b6588679ca8d7a8d0b9c49fc4c7874
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-30 20:14:19 +0300

    upd dependencies

--- src/test/java/StudentRegistrationFormTest.java
+++ src/test/java/StudentRegistrationFormTest.java
@@ -74,7 +74,7 @@ public class StudentRegistrationFormTest {
     @AfterMethod
     public void closeUp() {
         driver.close();
-        System.out.println("The close_up process is completed1");
+        System.out.println("The close_up process is completed");
         driver.quit();
         System.out.println("The quit process is completed");
     }
